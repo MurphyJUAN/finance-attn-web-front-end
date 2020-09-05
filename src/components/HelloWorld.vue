6c4c06d5eba781fdee6253c6fdb84c06c8ac4de3
Murphy_Juan <elin880508@gmail.com>
2020-09-05 10:37:21 +0800

    change with ys

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,9 +20,7 @@
         </b-dropdown> -->
         </h5>
         <h5 class="word-title">Word</h5>
-        <svg class="sort-btn" v-on:click="sortDiction" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z" /></svg>
+
 
     </div>
     <div class="flex">
@@ -71,176 +69,176 @@ import NavBar from './NavBar';
 import DataInfo from './data.json';
 
 export default {
-    name: 'HelloWorld',
-    components: {
-        BarChart,
-        DependencyGraph,
-        HeatMap,
-        BarChartWord,
-        NavBar,
-    },
-    data() {
-        return {
-            isSorted: false,
-            isChanged: false,
-            isHeatMapClick: false,
-            selectedHeatMapSentenceData: '',
-            selectedHeatMapGridData: [],
-            sentenceHeatMapGridData: [],
-            heatMapLabel: 'text',
-            color: {},
-            metaInfo: {},
-            barChart: {
-                data: [],
-                width: 540,
-                height: 500,
-                barHeight: 30,
-                barMargin: 20,
-            },
-            chartMargin: {
-                top: 20,
-                bottom: 20,
-                left: 5,
-                right: 5,
-            },
-            dependencyGraphDict: {
-                data: [],
-                width: 540,
-                height: 500,
-                barHeight: 30,
-                barMargin: 20,
-            },
-        };
-    },
-    methods: {
-        clickWord(idx, word) {
-            const targetId = this.dependencyGraphDict.data[idx].target;
-            const tmp = this.barChart.data[0];
-            let tmpA = {};
-            for (let i = 0; i < this.barChart.data.length; i += 1) {
-                if (this.barChart.data[i].name === targetId) {
-                    tmpA = this.barChart.data[i];
-                    break;
-                }
-            }
-            this.barChart.data[0] = tmpA;
-            this.barChart.data[targetId] = tmp;
-            this.isChanged = !this.isChanged;
+  name: 'HelloWorld',
+  components: {
+    BarChart,
+    DependencyGraph,
+    HeatMap,
+    BarChartWord,
+    NavBar,
+  },
+  data() {
+    return {
+      isSorted: false,
+      isChanged: false,
+      isHeatMapClick: false,
+      selectedHeatMapSentenceData: '',
+      selectedHeatMapGridData: [],
+      sentenceHeatMapGridData: [],
+      heatMapLabel: 'text',
+      color: {},
+      metaInfo: {},
+      barChart: {
+        data: [],
+        width: 540,
+        height: 500,
+        barHeight: 25,
+        barMargin: 7,
+      },
+      chartMargin: {
+        top: 20,
+        bottom: 20,
+        left: 5,
+        right: 5,
+      },
+      dependencyGraphDict: {
+        data: [],
+        width: 540,
+        height: 500,
+        barHeight: 25,
+        barMargin: 7,
+      },
+    };
+  },
+  methods: {
+    clickWord(idx, word) {
+      const targetId = this.dependencyGraphDict.data[idx].target;
+      const tmp = this.barChart.data[0];
+      let tmpA = {};
+      for (let i = 0; i < this.barChart.data.length; i += 1) {
+        if (this.barChart.data[i].name === targetId) {
+          tmpA = this.barChart.data[i];
+          break;
+        }
+      }
+      this.barChart.data[0] = tmpA;
+      this.barChart.data[targetId] = tmp;
+      this.isChanged = !this.isChanged;
 
-            const x = document.getElementById('text-0');
-            for (let i = 0; i < this.barChart.data[0].words.length; i += 1) {
-                if (this.barChart.data[0].words[i].word === word && i > 5) {
-                    const offset = (i - 5) * 85;
-                    x.scrollLeft = offset;
-                }
-            }
-        },
-        compareValue(a, b) {
-            if (a.value < b.value) {
-                return -1;
-            }
-            if (a.vlue > b.value) {
-                return 1;
-            }
-            return 0;
-        },
-        compareName(a, b) {
-            if (a.name < b.name) {
-                return -1;
-            }
-            if (a.name > b.name) {
-                return 1;
-            }
-            return 0;
-        },
-        // recoverDiction() {
-        //   this.barChart = JSON.parse(JSON.stringify(this.copyData));
-        //   this.isSorted = !this.isSorted;
-        //   for (let j = 0; j < this.dependencyGraphDict.links.length; j += 1) {
-        //     this.dependencyGraphDict.links[j].target = JSON.parse(JSON.stringify(this.dependencyLinkOrder[j].target));
-        //   }
-        // },
-        sortDiction() {
-            console.log('isSorted', this.isSorted);
-            // console.log(this.barChart);
-            if (!this.isSorted) {
-                this.barChart.data.sort(this.compareValue);
-            } else {
-                this.barChart.data.sort(this.compareName);
-            }
-            this.isSorted = !this.isSorted;
-            // console.log('----', this.dependencyGraphDict.links);
-            // // console.log('----', this.dependencyGraph.links);
-            // const tmp = [];
-            // for (let j = 0; j < this.dependencyGraphDict.links.length; j += 1) {
-            //   tmp.push(JSON.parse(JSON.stringify(this.dependencyGraphDict.links[j])));
-            // }
-            // // console.log('before:', this.dependencyGraph.links);
-            // for (let i = 0; i < this.barChart.data.length; i += 1) {
-            //   const newTarget = this.barChart.data[i].name;
-            //   console.log('newTarget', newTarget);
-            //   for (let j = 0; j < this.dependencyGraphDict.links.length; j += 1) {
-            //     console.log('=====', this.dependencyGraphDict.links[j].target, newTarget);
-            //     console.log(typeof (this.dependencyGraphDict.links[j].target), typeof (newTarget));
-            //     if (parseInt(this.dependencyGraphDict.links[j].target, 10) === parseInt(newTarget, 10)) {
-            //       console.log('opop');
-            //       tmp[j].target = i;
-            //       console.log(j, tmp[j].target);
-            //     }
-            //   }
-            // }
-            // console.log('tmp', tmp);
-            // this.dependencyGraphDict.links = tmp;
-        },
-        setHeatMapData(idx) {
-            this.heatMapLabel = 'modal';
-            this.selectedHeatMapGridData = this.barChart.data[idx].words;
-            this.selectedHeatMapSentenceData = this.barChart.data[idx].sentence;
-        },
-        colorFunc(idx, word) {
-            let a = this.color(1);
-            for (let j = 0; j < this.barChart.data[idx].words.length; j += 1) {
-                const targetItem = this.barChart.data[idx].words[j];
-                if (word.indexOf(targetItem.word) != -1) {
-                    a = this.color(targetItem.weight * 100);
-                    break;
-                }
-            }
-            console.log('a', a);
-            // if (a === 'rgb(247,255,255)') {
-            //   a = '#F9F7EB';
-            // }
-            return a;
-        },
-    },
-    mounted() {
-        if (DataInfo) {
-            this.barChart.data = DataInfo.sentencesData;
-            this.dependencyGraphDict.data = DataInfo.wordsData;
-            this.metaInfo = DataInfo.metaInfo;
+      const x = document.getElementById('text-0');
+      for (let i = 0; i < this.barChart.data[0].words.length; i += 1) {
+        if (this.barChart.data[0].words[i].word === word && i > 5) {
+          const offset = (i - 5) * 85;
+          x.scrollLeft = offset;
         }
-        let min_ = 1;
-        let max_ = 1;
-        for (let i = 0; i < this.barChart.data.length; i++) {
-            if (this.barChart.data[i].value < min_) {
-                min_ = this.barChart.data[i].value;
-            }
-            if (this.barChart.data[i].value > max_) {
-                max_ = this.barChart.data[i].value;
-            }
-        }
-        this.color = d3.scaleLinear()
-            .domain([min_ * 100, max_ * 100])
-            .range(['#F9F7EB', '#FF0001']);
-        console.log('min', min_, 'max', max_);
+      }
     },
+    compareValue(a, b) {
+      if (a.value > b.value) {
+        return -1;
+      }
+      if (a.vlue < b.value) {
+        return 1;
+      }
+      return 0;
+    },
+    compareName(a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    },
+    // recoverDiction() {
+    //   this.barChart = JSON.parse(JSON.stringify(this.copyData));
+    //   this.isSorted = !this.isSorted;
+    //   for (let j = 0; j < this.dependencyGraphDict.links.length; j += 1) {
+    //     this.dependencyGraphDict.links[j].target = JSON.parse(JSON.stringify(this.dependencyLinkOrder[j].target));
+    //   }
+    // },
+    sortDiction() {
+      console.log('isSorted', this.isSorted);
+      // console.log(this.barChart);
+      if (!this.isSorted) {
+        this.barChart.data.sort(this.compareValue);
+      } else {
+        this.barChart.data.sort(this.compareName);
+      }
+      this.isSorted = !this.isSorted;
+      // console.log('----', this.dependencyGraphDict.links);
+      // // console.log('----', this.dependencyGraph.links);
+      // const tmp = [];
+      // for (let j = 0; j < this.dependencyGraphDict.links.length; j += 1) {
+      //   tmp.push(JSON.parse(JSON.stringify(this.dependencyGraphDict.links[j])));
+      // }
+      // // console.log('before:', this.dependencyGraph.links);
+      // for (let i = 0; i < this.barChart.data.length; i += 1) {
+      //   const newTarget = this.barChart.data[i].name;
+      //   console.log('newTarget', newTarget);
+      //   for (let j = 0; j < this.dependencyGraphDict.links.length; j += 1) {
+      //     console.log('=====', this.dependencyGraphDict.links[j].target, newTarget);
+      //     console.log(typeof (this.dependencyGraphDict.links[j].target), typeof (newTarget));
+      //     if (parseInt(this.dependencyGraphDict.links[j].target, 10) === parseInt(newTarget, 10)) {
+      //       console.log('opop');
+      //       tmp[j].target = i;
+      //       console.log(j, tmp[j].target);
+      //     }
+      //   }
+      // }
+      // console.log('tmp', tmp);
+      // this.dependencyGraphDict.links = tmp;
+    },
+    setHeatMapData(idx) {
+      this.heatMapLabel = 'modal';
+      this.selectedHeatMapGridData = this.barChart.data[idx].words;
+      this.selectedHeatMapSentenceData = this.barChart.data[idx].sentence;
+    },
+    colorFunc(idx, word) {
+      let a = this.color(1);
+      for (let j = 0; j < this.barChart.data[idx].words.length; j += 1) {
+        const targetItem = this.barChart.data[idx].words[j];
+        if (word.indexOf(targetItem.word) != -1) {
+          a = this.color(targetItem.weight);
+          break;
+        }
+      }
+      console.log('a', a);
+      // if (a === 'rgb(247,255,255)') {
+      //   a = '#F9F7EB';
+      // }
+      return a;
+    },
+  },
+  mounted() {
+    if (DataInfo) {
+      this.barChart.data = DataInfo.sentencesData;
+      this.dependencyGraphDict.data = DataInfo.wordsData;
+      this.metaInfo = DataInfo.metaInfo;
+    }
+    // let min_ = 1;
+    // let max_ = 1;
+    // for (let i = 0; i < this.barChart.data.length; i++) {
+    //   if (this.barChart.data[i].value < min_) {
+    //     min_ = this.barChart.data[i].value;
+    //   }
+    //   if (this.barChart.data[i].value > max_) {
+    //     max_ = this.barChart.data[i].value;
+    //   }
+    // }
+    this.color = d3.scaleLinear()
+      .domain([0, 100])
+      .range(['rgb(255, 255, 255)', 'rgb(245, 91, 91)']);
+    // console.log('min', min_, 'max', max_);
+  },
 };
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Assistant:wght@400;600&display=swap');
 </style><style scoped>
- < !-- Add "scoped"attribute to limit CSS to this component only -->.title {
+.title {
      padding: 15px;
      margin: 0px;
  }
@@ -359,17 +357,17 @@ export default {
 
  .text {
      text-align: left;
-     /*height: 30px;*/
+     height: 25px;
      width: 100%;
      /* background: rgb(249,247,235);; */
      /* margin-top: 20px; */
      font-size: 18px;
      /* padding-top: 15px; */
-     margin-bottom: 6px;
+     margin-bottom: 7px;
      /* background: black; */
      overflow-x: scroll;
-     overflow-x: hidden;
-     /* overflow-y: hidden; */
+     /* overflow-x: hidden; */
+     overflow-y: hidden;
      /* text-overflow : ellipsis; */
      cursor: pointer;
      font-family: 'Assistant', sans-serif;
@@ -422,8 +420,9 @@ export default {
  }
 
  .word-text {
-     height: 30px;
-     margin-bottom: 10px;
+     font-size: 18px;
+     height: 25px;
+     margin-bottom: 7px;
      cursor: pointer;
  }
 
@@ -438,8 +437,10 @@ export default {
 
  .word-number {
      background: #f5a142;
+     font-size: 18px;
+     height: 25px;
      text-align: center;
-     margin-bottom: 20px;
+     margin-bottom: 7px;
      border-radius: 100%;
  }
 </style>
